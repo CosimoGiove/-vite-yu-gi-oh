47a16478b147379e06aa273c8c40159db3033fbf
cosimogiove <cosimo18497@gmail.com>
2023-12-12 17:16:10 +0100

    feat: store , esercizio finito

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script>
+import axios from "axios";
+import { store } from "./store";
 import header1 from './components/header1.vue';
 import main1 from './components/main1.vue';
-export default{
-  components:{
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
     header1,
     main1,
+  },
+  created() {
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=30&offset=0")
+      .then((data) => {
+        store.cards = data.data.data
+      })
   }
 }
 
--- src/components/card.vue
+++ src/components/card.vue
@@ -1,50 +1,45 @@
 <script>
-import axios from "axios";
-
+import { store } from "../store";
 export default {
     name: "card",
-    data(){
-        return{
-         cards:[],
+    data() {
+        return {
+            store,
         }
-    }, 
-    created(){
-        axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=30&offset=0")
-        .then((data)=>{
-          this.cards = data.data.data
-        })
-    }
+    },
 }
 </script>
 <template>
     <div class="contenitorecard">
         <div class="cardtrovate">
-            Found {{ cards.length }} card
+            Found {{ store.cards.length }} card
         </div>
         <div>
             <ul>
-                <li v-for="card in cards" class="nomecard">
+                <li v-for="card in store.cards" class="nomecard">
                     <img :src="card.card_images[0].image_url" alt="">
-                   <div> {{ card.name }}</div>
-                   <div class="tipologia">{{ card.archetype}}</div>
+                    <div> {{ card.name }}</div>
+                    <div class="tipologia">{{ card.archetype }}</div>
                 </li>
             </ul>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
-.contenitorecard{
+.contenitorecard {
     width: 83%;
     margin: 0 auto;
     padding-top: 45px;
     background-color: white;
-    .cardtrovate{
+
+    .cardtrovate {
         width: 90%;
         margin: 0 auto;
         background-color: #212529;
         padding: 20px 10px;
         color: white;
     }
+
     ul {
         display: flex;
         flex-wrap: wrap;
@@ -52,24 +47,27 @@ export default {
         margin: 0 auto;
         justify-content: space-between;
     }
-    li{
+
+    li {
         flex-basis: calc((100% / 5) - 20px);
         list-style: none;
         margin-bottom: 20px;
     }
-    img{
+
+    img {
         width: 100%;
     }
-    .nomecard{
-        background-color:#d48f38 ;
+
+    .nomecard {
+        background-color: #d48f38;
         color: white;
         text-align: center;
         text-transform: uppercase;
     }
-    .tipologia{
+
+    .tipologia {
         color: black;
         margin-top: 20px;
         padding-bottom: 20px;
     }
-}
-</style>
+}</style>
